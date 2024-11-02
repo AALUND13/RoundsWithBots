0efb649aeb2b5b3dcaa4baff39a6b3ffce6c7846
AALUND13 <tamlund1976@gmail.com>
2024-11-01 21:30:50 -0400

    Prevent bot spawning during ongoing battles

---
+++ RoundWithBot/Pacthes/PlayerAssignerPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using InControl;
+using ModdingUtils.AIMinion;
+using System.Collections;
+
+namespace RoundsWithBots.Pacthes {
+    [HarmonyPatch(typeof(PlayerAssigner))]
+    public class PlayerAssignerPatch {
+        [HarmonyPatch("CreatePlayer")]
+        public static bool Prefix(PlayerAssigner __instance, InputDevice inputDevice, bool isAI, ref IEnumerator __result) {
+            if(isAI 
+                && GameManager.instance.battleOngoing 
+                && !AIMinionHandler.sandbox
+            ) {
+                __result = EmptyEnumerator();
+                return false;
+            }
+            return true;
+        }
+
+        private static IEnumerator EmptyEnumerator() {
+            yield break;
+        }
+    }
+}
