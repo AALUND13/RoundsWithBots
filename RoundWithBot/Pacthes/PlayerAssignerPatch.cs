0997311c9920e35c9716f1ee71a420a26af6439d
AALUND13 <tamlund1976@gmail.com>
2024-11-03 00:54:11 -0400

    Prevent player spawn during ongoing battle

--- RoundWithBot/Pacthes/PlayerAssignerPatch.cs
+++ RoundWithBot/Pacthes/PlayerAssignerPatch.cs
@@ -8,8 +8,7 @@ namespace RoundsWithBots.Pacthes {
     public class PlayerAssignerPatch {
         [HarmonyPatch("CreatePlayer")]
         public static bool Prefix(PlayerAssigner __instance, InputDevice inputDevice, bool isAI, ref IEnumerator __result) {
-            if(isAI 
-                && GameManager.instance.isPlaying 
+            if( GameManager.instance.isPlaying 
                 && !AIMinionHandler.sandbox
             ) {
                 __result = EmptyEnumerator();
